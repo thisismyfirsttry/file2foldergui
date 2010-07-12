f8b9d97812f68e8cc708368edf37964a5bc997d3
jkleslie@gmail.com <jkleslie@gmail.com@9d35f785-0a12-17ea-f143-bc8154d09f50>
2010-07-12 14:41:13 +0000

    Version 1.4.4.0.  Updated release notes.

--- trunk/file2foldergui/file2foldergui/Form3.vb
+++ trunk/file2foldergui/file2foldergui/Form3.vb
@@ -5,8 +5,7 @@
     End Sub
 
     Private Sub Form3_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load  'latest release notes
-        txtboxReleaseNotes.Text = "Release notes for v1.4.3.0" _
-        & vbCrLf & vbCrLf & "* New logo." _
-        & vbCrLf & vbCrLf & "* Rudimentary multi-part file support.  Multi-part files can be ignored so that you can handle them yourself.  File names must contain cd, dvd, part, disc or disk followed by number (ex. movie-cd1.avi)."
+        txtboxReleaseNotes.Text = "Release notes for v1.4.4.0" _
+        & vbCrLf & vbCrLf & "* Fixed exception on update if no Internet connection."
     End Sub
 End Class
